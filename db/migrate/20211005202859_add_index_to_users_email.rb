4e3b35690cef674ead4e1493ce6cfca1126f5539
mulugeta16 <mulugeta2030@gmail.com>
2021-10-07 15:32:38 +0900

    comment

--- db/migrate/20211005202859_add_index_to_users_email.rb
+++ db/migrate/20211005202859_add_index_to_users_email.rb
@@ -1,9 +1,5 @@
-class User < ApplicationRecord
-  has_many :pictures
-  validates :name,  presence: true, length: { maximum: 30 }
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  before_validation { email.downcase! }
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+class AddIndexToUsersEmail < ActiveRecord::Migration[5.1]
+  def change
+    add_index :users, :email, unique: true
+  end
 end
